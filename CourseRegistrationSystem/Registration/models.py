0a0061b2671099e65c2a3a0c9022d4ef7759abf3
gülgün <glgn>
2016-04-29 16:01:03 +0300

    Models was implemented.

---
+++ CourseRegistrationSystem/Registration/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from model_utils import Choices
+from django.utils.translation import ugettext_lazy as _
+# Create your models here.
+
+class CourseInfo(models.Model):
+	DAYS = Choices(
+		(0, 'Monday', _('Monday')),
+		(1, 'Tuesday', _('Tuesday')),
+		(2, 'Wednesday', _('Wednesday')),
+		(3, 'Thursday', _('Thursday')),
+		(4, 'Friday', _('Friday')),
+		(5, 'Saturday', _('Saturday')))
+
+	start_time = models.TimeField()
+	end_time = models.TimeField()
+	code = models.CharField(max_length = 7)
+	instructor_name = models.CharField(max_length = 100)
+	day = models.IntegerField(choices = DAYS, default = DAYS.Monday)
+
+class Schedule(models.Model):
+	student_id = models.IntegerField()
+	comfirmed = models.BooleanField(default = False)
+	semester = models.ForeingKey(
+		'Semester',
+		on_delete = models.CASCADE)
+	courses = models.ManyToManyField(CourseInfo, related_name = 'schedule')
+
+	class Meta:
+		unique_together = (("student_id","semester"),)
+class Settings(models.Model):
+	on_registration = models.BooleanField(default = False)	
+
