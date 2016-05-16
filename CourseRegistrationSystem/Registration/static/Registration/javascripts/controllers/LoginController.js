9948c387858a5d1848aa77d3256614c37fe6c99f
gülgün <glgn>
2016-05-17 00:25:55 +0300

    Course factory was implemented

--- CourseRegistrationSystem/Registration/static/Registration/javascripts/controllers/LoginController.js
+++ CourseRegistrationSystem/Registration/static/Registration/javascripts/controllers/LoginController.js
@@ -18,7 +18,6 @@ app.controller("LoginController", ["$scope","StudentService","$window", "SocketS
         showAlert("Please enter Student No and password")
       }
     };
-
     var showAlert = function(message) {
       $("#alert-container").empty();
       $("#alert-container").append('<div id="alert" class="alert alert-warning alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Warning!</strong> '+ message +'</div>');
--- CourseRegistrationSystem/Registration/static/Registration/javascripts/services/CourseFactory.js
+++ CourseRegistrationSystem/Registration/static/Registration/javascripts/services/CourseFactory.js
@@ -1,19 +1,40 @@
-app.factory("CourseService",function($http, $q){
+app.factory("CourseService",function($http, $q){ 
 	var factory = {};
-	
 
-	
-	factory.getCourse = function(studentNo){
-
-	};	
-	
-	factory.filterCourse = function(cumulative){
-		
+	factory.getCourse = function(){
+	var deferred = $q.defer();
+		$http.post('/courseregistration',{ 
+			"method": "courses"	
+			}).then(function(data){
+				console.log(data);
+				if(data.status == 200 && data.data.success == true){
+					deferred.resolve(data.data);
+				} else {
+	        		deferred.reject({
+	          		success: false,
+	          		message: data.data.message
+	        	}, function(data) {
+	      			console.log(data);
+	      			if(data.status == 200){
+	      				deferred.reject(data.data)
+	      			}else{
+	      				deferred.reject({
+	      					success:false,
+	      					message: "Something went wrong"
+	      				});
+	      			}
+	    		});
+	      	}
+		});
 	};
+	
 	
 	factory.grouppingCourse = function(classID){
 
 	};
 
 	return factory;
-});
+}).factory('StudentFactory', function($http, $q){
+
+}
+)
